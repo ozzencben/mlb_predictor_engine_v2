0e5bdcaa55560b780e63364555590bb687cbe929
ozzencben <ozzencben@gmail.com>
2026-05-14 21:42:58 +0300

    added cron endpoints v2

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Tüm istekler için temel ayarlar
 const apiClient = axios.create({
-  baseURL: process.env.VITE_API_URL,  // "http://localhost:8000/api/v1"
+  baseURL: "https://mlb-predictor-engine-v2.onrender.com/api/v1",  // "http://localhost:8000/api/v1"
   headers: {
     'Content-Type': 'application/json',
   },
